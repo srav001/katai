8e7d46f46119347214817ce8341f9fe59a30ff09
Sravan <sravansuresh1x@gmail.com>
2024-02-10 15:06:21 +0800

    feat: re-build

--- src/lib/utilites.ts
+++ src/lib/utilites.ts
@@ -88,24 +88,3 @@ export function set(objectToUpdate: BasicObject, path: string, value: unknown) {
 export function get<T>(objectToRead: BasicObject, path: string) {
 	return getOrSetNestedValueInObject(objectToRead, path) as T extends null | undefined ? undefined : T;
 }
-
-/**
- * It takes a function and an argument, and if the function is an async function, it executes it with
- * the argument, otherwise it returns a resolved promise with the result of the function
- * @param {Function} func - The function to execute.
- * @param {T} arg - The argument to pass to the function.
- * @returns A promise that resolves to the result of the function.
- */
-// eslint-disable-next-line @typescript-eslint/ban-types
-export async function dynamicallyExecuteFunction<T = unknown>(func: Function, arg1: unknown, arg2: unknown) {
-	try {
-		const result = func(arg1, arg2);
-		if (result instanceof Promise) {
-			return (await result) as T;
-		} else {
-			return result as T;
-		}
-	} catch (err) {
-		throw new Error('Function failed to run', { cause: err });
-	}
-}
